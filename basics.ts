71b3504d264efaba4b8014944008064c801f458f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-15 14:18:39 +0100

    Update the basics.ts.

--- basics.ts
+++ basics.ts
@@ -76,3 +76,23 @@ peopleArray = [
     age: 27,
   },
 ];
+
+// Functions and types
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+// updatedArray[0].split('');  // error
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd'); // ['d', 'a', 'b', 'c']
