b3c079622d618b9fdcc879d4cf0a3de90c0b7cb9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-02-16 16:56:30 +0100

    Add Todo component

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Todos items={todos} />
     </div>
   );
 }
